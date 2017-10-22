69d9e7b1e6ce256cc69bfdde5e2adbdf010d883b
Krzysiu <kheciek@gmail.com>
2017-10-22 16:16:45 +0200

    Formatting fixed

--- App.js
+++ App.js
@@ -1,45 +1,48 @@
 var baseUrl = 'https://kodilla.com/pl/bootcamp-api';
 var myHeaders = {
-  'X-Client-Id': '2349',
-  'X-Auth-Token': 'fff16c44c42316ba3de0fa0bfd6f0217'
+	'X-Client-Id': '2349',
+	'X-Auth-Token': 'fff16c44c42316ba3de0fa0bfd6f0217'
 };
 $.ajaxSetup({
 	headers: myHeaders
 });
 $.ajax({
-    url: baseUrl + '/board',
-    method: 'GET',
-    success: function(response) {
-      setupColumns(response.columns);
-    }
+	url: baseUrl + '/board',
+	method: 'GET',
+	success: function(response) {
+		setupColumns(response.columns);
+	}
 });
+
 function setupColumns(columns) {
-    columns.forEach(function (column) {
-  		var col = new Column(column.id, column.name);
-        board.createColumn(col);
-        setupCards(col, column.cards);
-    });
+	columns.forEach(function(column) {
+		var col = new Column(column.id, column.name);
+		board.createColumn(col);
+		setupCards(col, column.cards);
+	});
 }
+
 function setupCards(col, cards) {
-	cards.forEach(function (card) {
-        var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
-    	col.createCard(card);
-  	})
+	cards.forEach(function(card) {
+		var card = new Card(card.id, card.name, card.bootcamp_kanban_column_id);
+		col.createCard(card);
+	})
 }
 Column.prototype = {
 	createCard: function(card) {
-	  this.element.children('ul').append(card.element);
+		this.element.children('ul').append(card.element);
 	},
 	deleteColumn: function() {
-	  this.element.remove();
+		this.element.remove();
 	}
 };
 // OGÓLNA FUNKCJA
 function randomString() {
 	var chars = '0123456789abcdefghiklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXTZ'.split();
-	var str = '', i;
+	var str = '',
+		i;
 	for (i = 0; i < 10; i++) {
-	  str += chars[Math.floor(Math.random() * chars.length)];
+		str += chars[Math.floor(Math.random() * chars.length)];
 	}
 	return str;
 }
